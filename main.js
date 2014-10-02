a47b9c237b9d32d5b3d66ee6771fab179fcbb8bf
Jordan Curzon <curzonj@gmail.com>
2014-10-02 08:31:57 -0700

    updates

--- main.js
+++ main.js
@@ -2,37 +2,95 @@
 
 var Q = require('q');
 var qhttp = require("q-io/http");
+var uuidGen = require('node-uuid');
+
+Q.longStackSupport = true;
 
 var _endpointCache;
 var _authCache;
 
 module.exports = {
-    request: function (endpoint, method, expects, path, body) {
+    authorize_req: function (req, restricted) {
+        var auth_header = req.headers.authorization;
+
+        if (auth_header === undefined) {
+            // We do this so that the Q-promise error handling
+            // will catch it
+            return Q.fcall(function() {
+                throw new Error("not authorized");
+            });
+        }
+
+        var parts = auth_header.split(' ');
+
+        // TODO make a way for internal apis to authorize
+        // as a specific account without having to get a
+        // different bearer token for each one. Perhaps
+        // auth will return a certain account if the authorized
+        // token has metadata appended to the end of it
+        // or is fernet encoded.
+        if (parts[0] != "Bearer") {
+            throw new Error("not authorized");
+        }
+
+        // This will fail if it's not authorized
+        return qhttp.read({
+            method: "POST",
+            url: process.env.AUTH_URL + '/token',
+            headers: {
+                'X-Request-ID': uuidGen.v1(),
+                "Content-Type": "application/json"
+            },
+            body: [JSON.stringify({
+                token: parts[1],
+                restricted: (restricted === true)
+            })]
+        }).then(function(body) {
+            return JSON.parse(body.toString());
+        }).fail(function(e) {
+            throw new Error("not authorized");
+        });
+    },
+    // BOBSUNCLE
+    request: function (endpoint, method, expects, path, body, opts) {
+        if (opts === undefined) {
+            opts = {}
+        }
+
         return Q.spread([this.getEndpoints(), this.getAuthToken()], function(endpoints, token) {
+            var bearer = token;
+
+            if (opts.sudo_account !== undefined) {
+                bearer += '/' + opts.sudo_account;
+            }
+
             return qhttp.request({
                 method: method,
                 url: endpoints[endpoint] + path,
                 headers: {
-                    "Authorization": "Bearer " + token,
+                    'X-Request-ID': uuidGen.v1(),
+                    "Authorization": "Bearer " + bearer,
                     "Content-Type": "application/json"
                 },
                 body: ( body === undefined ? [] : [JSON.stringify(body)])
             }).then(function(resp) {
                 if (resp.status !== expects) {
-                    resp.body.read().then(function(b) {
-                        console.log(endpoint+" " + resp.status + " reason: " + b.toString());
-                    }).done();
-
-                    throw new Error(endpoint+" responded with " + resp.status);
-                } else {
                     return resp.body.read().then(function(b) {
-                        try {
-                            return JSON.parse(b.toString());
-                        } catch(e) {
-                            console.log(e);
-                            return b;
-                        }
-                    });
+                        console.log(endpoint+" " + resp.status + " reason: " + b.toString());
+
+                        throw new Error(endpoint+" responded with " + resp.status);
+                    }).done();
+                } else {
+                    if (resp.status !== 204) {
+                        return resp.body.read().then(function(b) {
+                            try {
+                                return JSON.parse(b.toString());
+                            } catch(e) {
+                                console.log('invalid json from %s: `%s`', endpoint, b.toString());
+                                return b;
+                            }
+                        });
+                    }
                 }
             });
         });
@@ -46,6 +104,7 @@ module.exports = {
                 return qhttp.read({
                     url: process.env.SPODB_URL + '/endpoints',
                     headers: {
+                        'X-Request-ID': uuidGen.v1(),
                         "Content-Type": "application/json",
                     }
                 }).then(function(b) {
@@ -69,6 +128,7 @@ module.exports = {
                 return qhttp.read({
                     url: endpoints.auth + '/auth?ttl=3600',
                     headers: {
+                        'X-Request-ID': uuidGen.v1(),
                         "Content-Type": "application/json",
                         "Authorization": 'Basic ' + new Buffer(process.env.INTERNAL_CREDS).toString('base64')
                     }
@@ -85,7 +145,7 @@ module.exports = {
     },
 
     getAuthToken: function() {
-        this.getAuth().then(function(auth) {
+        return this.getAuth().then(function(auth) {
             return auth.token;
         });
     }
